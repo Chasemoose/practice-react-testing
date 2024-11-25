bb0e2609140299eac7d2d7b5052ddac53327a5bf
Jaro <jaroswidnik@interia.pl>
2024-11-25 11:16:03 +0100

    Another test for ex2

--- src/components/LoginForm.test.js
+++ src/components/LoginForm.test.js
@@ -1,29 +1,45 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import LoginForm from './LoginForm'
-import Catcher from './Catcher'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import LoginForm from "./LoginForm";
+import Catcher from "./Catcher";
 
+describe("<LoginForm", () => {
+	test("loginInput - long value", async () => {
+		render(<LoginForm />);
 
+		const fieldLogin = await screen.findByRole("textbox", { name: /login/i });
+		userEvent.type(fieldLogin, "ka");
 
-describe('<LoginForm', () => {
-    test('loginInput - long value', async () => {
-        render(<LoginForm/>)
+		const error = await screen.findByText("The field is too short!");
+		expect(error).toBeInTheDocument();
+	});
+	test("submit incorrect data #1", async () => {
+		const mock = jest.fn();
+		mock.mockReturnValueOnce(false);
+		render(
+			<Catcher>
+				<LoginForm tryAuth={mock} />
+			</Catcher>
+		);
 
-        const fieldLogin = await screen.findByRole('textbox', {name: /login/i})
-        userEvent.type(fieldLogin, 'ka')
+		const button = await screen.findByRole("button");
+		userEvent.click(button);
 
-        const error = await screen.findByText('The field is too short!')
-        expect(error).toBeInTheDocument()
-    })
-    test('submit incorrect data', async () => {
-        const mock = jest.fn()
-        mock.mockReturnValueOnce(false)
-        render(<Catcher><LoginForm tryAuth={mock}/></Catcher>)
+		const error = await screen.findByText("Jest błąd!");
+		expect(error).toBeInTheDocument();
+	});
 
-        const button = await screen.findByRole('button')
-        userEvent.click(button)
+	test("submit incorrect data #2", async () => {
+		const mock = jest.fn();
+		mock.mockReturnValueOnce(false);
+		render (<LoginForm tryAuth={mock} />)
 
-        const error = await screen.findByText('Jest błąd!')
-        expect(error).toBeInTheDocument()
-    })
-} )
+		const button = await screen.findByRole("button");
+
+        try {
+            userEvent.click(button);
+        } catch(error) {
+            expect(error.message).toBe('Incorrect data!');
+        }
+	});
+});
